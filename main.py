e0894c16b18d0451abc8b512ae8063f9566deafe
NealGuides <54384069+NealGuides@users.noreply.github.com>
2025-02-19 10:53:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -103,4 +103,4 @@ def tactic_code_api():
 
 # ✅ Flask Server Configuration
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
